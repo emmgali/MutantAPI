4245895bc4465453128f2ed616ffb5ff10ebfbba
Trekkar <noggaxiii@gmail.com>
2018-09-23 18:51:51 -0300

    final fix

--- app.js
+++ app.js
@@ -6,33 +6,7 @@ app.use(express.json());
 
 var mutantLogic = require("./mutantLogic.js");
 
-/*
-const { Pool } = require('pg');
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: true
-});
-*/
-
-/*
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-})
-*/
-
 app.post('/mutant/', function (req, res) {
-
-//console.log("el body es:")
-//console.log(req.body);
 
   if(mutantLogic.detectMutant(req.body.dna)){
   	res.sendStatus(200);
@@ -53,3 +27,4 @@ app.get('/hola', function(req, res){
 app.get('/intro', function(req, res){
 	res.send('Hola Martin! Gracias por usar esta aplicación!');	
 })
+
