849bcb50a6722f3bad92163bb2806897840cf1a2
Trekkar <noggaxiii@gmail.com>
2018-09-23 18:04:35 -0300

    postgresql

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ app.use(express.json());
 
 var mutantLogic = require("./mutantLogic.js");
 
+const { Pool } = require('pg');
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+});
+
+.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+})
+
+
 app.post('/mutant/', function (req, res) {
 
 //console.log("el body es:")
