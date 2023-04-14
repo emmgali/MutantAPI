a02db25dccf6cff98668cf48dd7fbc8fb95e8735
Emiliano G <noggaxiii@gmail.com>
2023-04-14 08:46:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -23,8 +23,3 @@ app.listen(PORT, function(){
 app.get('/hola', function(req, res){
 	res.send('que tal?');	
 })
-
-app.get('/intro', function(req, res){
-	res.send('Hola Martin! Gracias por usar esta aplicación!');	
-})
-
